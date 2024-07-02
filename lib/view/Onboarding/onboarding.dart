3ef32e26499ffc20e94c14197bbc82b4a47d29b8
AimenArshad <95518409+AimenArshad@users.noreply.github.com>
2024-07-02 19:47:04 +0500

    changes done

--- lib/view/Onboarding/onboarding.dart
+++ lib/view/Onboarding/onboarding.dart
@@ -21,88 +21,95 @@ class OnboardingScreen extends StatefulWidget {
 
 class OnboardingScreenState extends State<OnboardingScreen> {
   @override
-  void initState() {
-    super.initState();
-    // Add any initialization logic here
-  }
-
-  @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        body: Stack(
           children: [
-            Expanded(
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      'assets/StarX Vpn Light Mode/Illustration/Illustration 1.png',
-                      height: 200.h, // Responsive height
-                      width: 200.w, // Responsive width
-                      fit: BoxFit.cover,
-                    ),
-                    SizedBox(height: 20.h),
-                    Container(
-                      width: 200.w, // Same width as the image
-                      child: Text(
-                        'Keep your online activity private, even in public.',
-                        textAlign: TextAlign.center,
-                        style: CustomTextStyle.onboardingText.copyWith(
-                          fontSize: 18.sp, // Responsive font size
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 20.h),
-                    Image.asset(
-                      'assets/StarX Vpn Light Mode/Swipe/Swipe.png',
-                      height: 20.h,
-                      width: 40.w,
-                      fit: BoxFit.cover,
-                    ),
-                    SizedBox(height: 30.h), // Responsive height
-                    Image.asset(
-                      'assets/StarX Vpn Light Mode/Swipe/Button 1.png',
-                      height: 50.h,
-                      width: 50.w,
-                      fit: BoxFit.cover,
-                    ),
-                    SizedBox(height: 20.h),
-                  ],
-                ),
+            // Background image positioned behind the main content
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: Image.asset(
+                'assets/StarX Vpn Light Mode/Map Illustration/Group.png',
+                fit: BoxFit.cover,
               ),
             ),
-            Container(
-              padding:
-                  EdgeInsets.symmetric(horizontal: 16.w), // Padding for buttons
-              width: double.infinity,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  MyButton(
-                    text: 'Start Free Trial',
-                    onTap: () {
-                      // Handle button press
-                      print('Button 1 pressed');
-                    },
-                    isPrimary: true,
-                    width: double.infinity, // Full width button
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/StarX Vpn Light Mode/Illustration/Illustration 1.png',
+                          height: 200.h, // Responsive height
+                          width: 200.w, // Responsive width
+                          fit: BoxFit.cover,
+                        ),
+                        SizedBox(height: 20.h),
+                        Container(
+                          width: 200.w, // Same width as the image
+                          child: Text(
+                            'Keep your online activity private, even in public.',
+                            textAlign: TextAlign.center,
+                            style: CustomTextStyle.onboardingText.copyWith(
+                              fontFamily: 'Satoshi',
+                              fontSize: 22.sp, // Responsive font size
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20.h),
+                        Image.asset(
+                          'assets/StarX Vpn Light Mode/Swipe/Swipe.png',
+                          height: 20.h,
+                          width: 40.w,
+                          fit: BoxFit.cover,
+                        ),
+                        SizedBox(height: 30.h), // Responsive height
+                        Image.asset(
+                          'assets/StarX Vpn Light Mode/Swipe/Button 1.png',
+                          height: 50.h,
+                          width: 50.w,
+                          fit: BoxFit.cover,
+                        ),
+                        SizedBox(height: 20.h),
+                      ],
+                    ),
                   ),
-                  SizedBox(height: 10.h),
-                  MyButton(
-                    text: 'Sign In',
-                    onTap: () {
-                      // Handle button press
-                      print('pressed');
-                    },
-                    isPrimary: false,
-                    width: double.infinity, // Full width button
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 16.w),
+                  width: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      MyButton(
+                        text: 'Start Free Trial',
+                        onTap: () {
+                          print('Button 1 pressed');
+                        },
+                        isPrimary: true,
+                        width: double.infinity,
+                      ),
+                      SizedBox(height: 10.h),
+                      MyButton(
+                        text: 'Sign In',
+                        onTap: () {
+                          print('pressed');
+                        },
+                        isPrimary: false,
+                        width: double.infinity,
+                      ),
+                      SizedBox(height: 10.h),
+                    ],
                   ),
-                  SizedBox(height: 10.h),
-                ],
-              ),
+                ),
+              ],
             ),
           ],
         ),
